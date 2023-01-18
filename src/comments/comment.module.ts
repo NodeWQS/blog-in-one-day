8c181bad60c51ad8d49ef70bbc4e0a81eb216d05
Mesrop Araqelyan <95045783+NodeWQS@users.noreply.github.com>
2023-01-18 14:37:07 +0400

    Update comment.module.ts

--- src/comments/comment.module.ts
+++ src/comments/comment.module.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import controller from "./comment.controller";
 
 const router = Router();
-router.post('/', controller.addPost);
-router.delete('/:id', controller.deletePost);
+router.post('/', controller.addComment);
+router.delete('/:id', controller.deleteComment);
 
 module.exports = router;
